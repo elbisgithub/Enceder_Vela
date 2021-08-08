c59357f3728e1b53b22e17c8784074c09a36d57b
elbisgithub <aluprof.elbismarmol@gmail.com>
2021-08-08 16:05:01 -0500

    Se agrego la funcion encenderVela()

--- verificarVela.js
+++ verificarVela.js
@@ -20,30 +20,34 @@ function validarStatus()
 }
 
 
-
 function encenderVela()
 {
-  var encendedor=document.getElementById('id');
+  if(document.getElementById('cerillos').checked)
+    {
+     document.write
+     ("¡Felicidades! Encendiste la Vela"
+     +"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/><br><br>");
+     document.write
+     ("<br>"+"Con unos buenos Cerillos"
+     +"<br><br><img width='150' height='150' src='E:/PLATZI/Enceder_Vela/cerillo.png'/><br><br>");
+   }
+   else if (document.getElementById('mechera').checked)
+    {
+      document.write
+      ("¡Felicidades! Encendiste la Vela"
+      +"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/>");
+      document.write
+      ("<br>"+"Con una buena Mechera"
+      +"<br><br><img width='150' height='150' src='E:/PLATZI/Enceder_Vela/mechera.png'/><br><br>");
+   }
+   else
+    {
+     document.write
+     ("¡Felicidades! Encendiste la Vela"
+     +"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/>");
+     document.write
+     ("<br>"+"Con un Buen Fuego"
+      +"<br><br><img width='150' height='150' src='E:/PLATZI/Enceder_Vela/fire.png'/><br><br>");
+   }
 
-  switch (encendedor)
-  {
-    case cerillos.checked:
-    document.write
-    ("¡Felicidades! Encendiste la Vela"+"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/>");
-    document.write
-    ("Con unos buenos Cerillos");
-    break;
-    case mechera.checked:
-      document.write
-      ("¡Felicidades! Encendiste la Vela"+"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/>");
-      document.write
-      ("Con una buena Mechera");
-    break;
-    default:
-    document.write
-    ("¡Felicidades! Encendiste la Vela"+"<br><br><img width='150' height='250' src='E:/PLATZI/Enceder_Vela/flameless-candle-encend.png'/>");
-    document.write
-    ("Con un buen Fuego");
-    break;
-  }
 }
